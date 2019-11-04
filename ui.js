5942c9370225f67682c8dcfaec791589d831c5a4
ilopX <ilopXgroup@gmail.com>
2019-11-04 12:58:26 +0200

    add(ui): even binding for html

---
+++ ui.js
@@ -0,0 +1,32 @@
+let ui = {
+    length: document.getElementById('length'),
+    container: document.getElementById('container'),
+    renderType: document.getElementById('renderType'),
+    btnCreate: document.getElementById('btnCreate'),
+    btnPlay: document.getElementById('btnPlay'),
+    btnClear: document.getElementById('btnClear'),
+    btnRender: document.getElementById('btnRender')
+}
+export default ui
+
+ui.btnCreate.onclick = () => {
+
+}
+
+ui.renderType.onchange = () => {
+
+} 
+ui.btnRender.onclick = () => {
+
+}
+
+btnPlay.onclick = () => {
+
+}
+
+
+
+
+
+
+
