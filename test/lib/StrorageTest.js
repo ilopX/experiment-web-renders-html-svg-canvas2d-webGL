6299109d6f767db3e100748e4ba1ac0cec1158e4
ilopX <ilopXgroup@gmail.com>
2019-11-08 12:33:30 +0200

    add(Storage): for save ui data

---
+++ test/lib/StrorageTest.js
@@ -0,0 +1,21 @@
+import { Storage } from './../../lib/Storage.js'
+
+const equal = chai.assert
+
+describe('StorageTest', function () {
+    it('save-load property', function () {
+        let storage = new Storage('fakeId', true)
+
+        let value = 'beginValue'
+        const fakeProperty = {
+            name: 'fakeName',
+            get: () => value,
+            set: (val) => value = val
+        }
+        storage.save(fakeProperty)
+        value = 'changedValue'
+        storage.load(fakeProperty)
+        equal(fakeProperty.get() == 'beginValue')
+    })
+
+})
