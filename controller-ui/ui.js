a25e3da42d754e95ae2fddac58ab8de5cba950fb
ilopX <ilopXgroup@gmail.com>
2019-11-09 19:56:44 +0200

    feat(ui): show frameInfo in table

--- controller-ui/ui.js
+++ controller-ui/ui.js
@@ -2,6 +2,36 @@ import Controller from './Controller.js'
 import PropertyUiComponent from '../lib/PropertyUiComponent.js'
 import Storage from '../lib/Storage.js'
 
+class TableValues {
+    constructor(parentElement) {
+        this._parentElem = parentElement
+        this._values = {}
+    }
+
+    initNames(...names) {
+        this._parentElem.innerHTML = ''
+        let tableElem = document.createElement('table')
+        for (const name of names) {
+            let tr = document.createElement('tr')
+            let td = document.createElement('td')
+            let tdVal = document.createElement('td')
+            td.innerText = name
+            tr.appendChild(td)
+            tr.appendChild(tdVal)
+            tableElem.appendChild(tr)
+            this._values[name] = tdVal
+        }
+        this._parentElem.appendChild(tableElem)
+    }
+
+    setValues(values) {
+        for (const key of Object.keys(values)) {
+                const val = values[key]
+                this._values[key].innerText = val.toFixed(2)
+        }
+    }
+}
+
 let ui = {
     length: document.getElementById('length'),
     container: document.getElementById('container'),
@@ -9,33 +39,33 @@ let ui = {
     animationType: document.getElementById('animationType'),
     btnCreate: document.getElementById('btnCreate'),
     btnPlay: document.getElementById('btnPlay'),
-    btnClear: document.getElementById('btnClear'),
-    btnRender: document.getElementById('btnRender'),
-    fps: document.getElementById('fps'),
-    animationProperties: document.getElementById('animationProperties')
+    frameInfo: document.getElementById('frameInfo'),
+    animationProperties: document.getElementById('animationProperties'),
+    frameInfo1: new TableValues(document.getElementById('frameInfo'))
 }
 
 let controller = new Controller(ui.container)
 
-controller.getRenders( (name) => {
+controller.getRenders((name) => {
     let opt = document.createElement('option')
     opt.innerText = name
     renderType.appendChild(opt)
 })
 
-controller.getAnimations( (name) => {
+controller.getAnimations((name) => {
     let opt = document.createElement('option')
     opt.innerText = name
     animationType.appendChild(opt)
 })
 
 controller.onReady(() => {
+    ui.frameInfo1.initNames('renderTime', 'idleTime', 'fps', 'multiRate')
+
     controller.setRender(ui.renderType.value)
-    // controller.setAnimationAlgorithm(ui.animationType.value)
     controller.createObjects(1000)
     ui.length.value = 1000
-    controller.setAnimationAlgorithm('Bounce')
-    ui.animationType.value = 'Bounce'
+    controller.setAnimationAlgorithm('Snow')
+    ui.animationType.value = 'Snow'
     controller.setRender('Canvas 2d Render')
     ui.renderType.value = 'Canvas 2d Render'
     controller.play()
@@ -43,12 +73,14 @@ controller.onReady(() => {
 
 // controller event
 controller.onPlayEvent((start) => {
-    btnPlay.innerText = start 
-        ? 'Pause' 
+    btnPlay.innerText = start
+        ? 'Pause'
         : 'Play'
 })
 
-controller.onFpsUpdate((fps) => ui.fps.innerText = fps)
+controller.onFpsUpdate((frameInfo) => {
+    ui.frameInfo1.setValues(frameInfo)
+})
 
 controller.onAnimationChange((animation) => {
     ui.animationProperties.innerHTML = ''
@@ -56,8 +88,11 @@ controller.onAnimationChange((animation) => {
     let storage = new Storage(id, isSaveAll)
     animation.properties.properties.forEach((prop) => {
         let component = new PropertyUiComponent(prop, storage)
-        for(let elem of component.elements) {
-            ui.animationProperties.appendChild(elem)}
+        let div = document.createElement('div')
+        component.elements.forEach((elem) => {
+            div.appendChild(elem)
+        })
+        ui.animationProperties.appendChild(div)
     })
 })
 
@@ -76,10 +111,6 @@ ui.renderType.onchange = () => {
 
 ui.animationType.onchange = () => {
     controller.setAnimationAlgorithm(ui.animationType.value)
-} 
-
-ui.btnRender.onclick = () => {
-    controller.rendering()
 }
 
 ui.btnPlay.onclick = () => {
